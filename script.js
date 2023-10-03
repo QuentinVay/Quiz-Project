78a7029efd75ae5db1f21639f1d78c8574567a4c
Adri-bis <adrien.ma.16@hotmail.fr>
2023-10-03 17:16:31 +0200

    ajout de js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let marginLeft;
 function createElement() {
   const elementLeft = document.createElement("div");
   elementLeft.className = "chocolatine";
-  marginLeft =(Math.random() * 50) + (Math.random()*20);
+  marginLeft = (Math.random() * 50) + (Math.random() * 20);
   elementLeft.style.marginLeft = marginLeft + "%";
-  elementLeft.style.animationDuration=Math.random()*4 + 3+'s';
-  
+  elementLeft.style.animationDuration = Math.random() * 4 + 3 + 's';
+
   const elementRight = document.createElement("div");
   elementRight.className = "chocolatine";
   marginLeft = Math.max((Math.random() * 60) + 20, 40);
   elementRight.style.marginLeft = marginLeft + "%";
-  elementRight.style.animationDuration=Math.random()*4 + 3+'s';
+  elementRight.style.animationDuration = Math.random() * 4 + 3 + 's';
 
   body.appendChild(elementLeft);
   body.appendChild(elementRight);
@@ -28,3 +28,10 @@ function createElement() {
   }, 3000);
 }
 setInterval(createElement, 700);
+
+let aboutQ = document.querySelector('.aboutPhotoHome');
+const aboutLq = document.querySelector('.aboutPhotoHomea')
+
+aboutQ.addEventListener('click', function () {
+  aboutLq.classList.toggle("visible")
+})
