75f9aa962c0bd76d2abdd85bb7b045a953bf69b9
Adri-bis <adrien.ma.16@hotmail.fr>
2023-10-04 12:01:58 +0200

    navbar finis

--- script.js
+++ script.js
@@ -73,25 +73,25 @@ function generateRandomAnswer(quention, choices, correctAnswer, fact) {
   // addEventListener sur chaque button de réponse
   const verificationAnswer = document.getElementsByClassName(`answerChoice${index}`);
   const arrayAnswerButton = Array.from(verificationAnswer);
-  arrayAnswerButton.forEach((element,buttonIndex) => {
-      element.addEventListener('click', () => {
-        arrayAnswerButton.forEach(button => {
-          button.style.pointerEvents='none';
-        })
-        // verifier la réponse de lutilisateur avec la bonne réponse de la question
-        // selon résultat contour rouge ou vert sur la réponse de lutilisateur
-        if (choices[buttonIndex]===correctAnswer){
-          element.style.border="4px solid green";
-        }else{
-          element.style.border="4px solid red";
-        }
-        // attendre 3s appel function selectionner question
-    setTimeout(() => {
-      selectquestionquiz(questionsVariees);
-    }, 2000);
-    
+  arrayAnswerButton.forEach((element, buttonIndex) => {
+    element.addEventListener('click', () => {
+      arrayAnswerButton.forEach(button => {
+        button.style.pointerEvents = 'none';
+      })
+      // verifier la réponse de lutilisateur avec la bonne réponse de la question
+      // selon résultat contour rouge ou vert sur la réponse de lutilisateur
+      if (choices[buttonIndex] === correctAnswer) {
+        element.style.border = "4px solid green";
+      } else {
+        element.style.border = "4px solid red";
+      }
+      // attendre 3s appel function selectionner question
+      setTimeout(() => {
+        selectquestionquiz(questionsVariees);
+      }, 2000);
+
+    });
   });
-});
 }
 // Demarrage du quiz init temps à 60s chronometre
 let temps = 60;
@@ -107,7 +107,7 @@ function selectquestionquiz(tableauquestion) {
   let randomIndex = Math.floor(Math.random() * tableauquestion.length);
   const { question, choices, correctAnswer, fact } =
     tableauquestion[randomIndex];
-    // afficher la question, stocker la bonne reponse dans une variable
+  // afficher la question, stocker la bonne reponse dans une variable
   generateRandomAnswer(question, choices, correctAnswer, fact);
   // supprimer la question du tableau afin de ne pas repeter la question
   tableauquestion.splice(randomIndex, 1);
@@ -135,3 +135,10 @@ playAgainButton.addEventListener("click", function () {
   quizSection.classList.add("visible");
 });
 
+let navbarflo = document.querySelector('.burgermenu');
+let navbarHid = document.querySelector('.navBarHidden')
+
+navbarflo.addEventListener('click', function () {
+  navbarHid.classList.toggle("visible");
+  navbarflo.classList.toggle("tester")
+})
