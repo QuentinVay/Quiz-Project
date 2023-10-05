c72fff427dda6efd6b485f7f0b8d9242f9af33e6
Marcelo <marceloromero2016ar@gmail.com>
2023-10-05 11:54:46 +0200

    fix bug timer

--- script.js
+++ script.js
@@ -1,97 +1,91 @@
 import questionsVariees from "./questions.js";
 
+const affichageRank = document.getElementById('rankSelection');
+const affichageScoreRank = document.querySelector('.detailsRankUserName');
+
+const bodySectionQuiz = document.querySelector(".bodyquiz");
 const body = document.querySelector(".bodyRank");
+const startQuiz = document.querySelector('.buttonStartHome');
+
 const imgTrophy = document.querySelector(".imgTrophy");
 const imgShame = document.querySelector(".imgShame");
+
 let marginLeft;
 let tempsTimer = 60;
 let timeprogress;
-let nombrepoints = 130;
+let tempsRestant;
+let nombrepoints = 0;
 let nameUser;
+let progressBar;
+let buttonsEnable;
+let endQuiz = false
 
-const startQuiz = document.querySelector('.buttonStartHome');
+function initialisationQuiz(){
+  bodySectionQuiz.innerHTML='';
+  tempsTimer=60;
+  compteur=0;
+  index = 0;
+  nombrepoints=0;
+  affichageRank.style.display="none";
+  imgTrophy.style.display = "none";
+  imgShame.style.display = "none";
+}
 startQuiz.addEventListener('click', ()=>{
-  // Demarrage du quiz init temps à 60s chronometre
- nameUser= document.getElementById('pseudo').value;
-  startChronometre(tempsTimer);
-  selectquestionquiz(questionsVariees);
+  initialisationQuiz();
+// Demarrage du quiz init temps à 60s chronometre
+  nameUser= document.getElementById('pseudo').value;
+  buttonsEnable = selectquestionquiz(questionsVariees); 
+  tempsRestant=startChronometre(tempsTimer,buttonsEnable);
 });
 
-if (nombrepoints >= 100) {
-  imgTrophy.style.display = "block";
-  imgShame.style.display = "none";
-  function createElementChoco() {
-    const elementLeft = document.createElement("div");
-    elementLeft.className = "chocolatine";
+function createPastry(className) {
+  const element = document.createElement("div");
+  element.className = className;
+  element.style.marginLeft = Math.random() * 90 + "%";
+  element.style.animationDuration = Math.random() * 4 + 3 + "s";
+  return element;
+}
 
-    marginLeft = Math.random() * 50;
-    elementLeft.style.marginLeft = marginLeft + "%";
-    elementLeft.style.animationDuration = Math.random() * 4 + 3 + "s";
+function showRank() {
+  const affichageRank = document.getElementById('rankSelection');
+  const affichageScoreRank = document.querySelector('.detailsRankUserName');
+  endQuiz=true;
 
-    const elementRight = document.createElement("div");
-    elementRight.className = "chocolatine";
-    marginLeft = Math.max(Math.random() * 60 + 20, 30);
-    elementRight.style.marginLeft = marginLeft + "%";
-    elementRight.style.animationDuration = Math.random() * 4 + 3 + 's';
-
-    body.appendChild(elementLeft);
-    body.appendChild(elementRight);
-    
-    elementLeft.addEventListener("click", function () {
-      elementLeft.remove();
-    });
-    elementRight.addEventListener("click", function () {
-      elementRight.remove();
-    });
-    setTimeout(function () {
-      elementLeft.remove();
-      elementRight.remove();
-    }, 3000);
+  if (nombrepoints >= 100) {
+    // Cas où nombrepoints est supérieur ou égal à 100 (team chocolatine)
+    affichageScoreRank.innerHTML = `<p>Félicitations ${nameUser} tu fais partie de la team chocolatine! ton score est de :${nombrepoints}</p>`;
+    imgTrophy.style.display = "block";
+  } else {
+    // Cas où nombrepoints est inférieur à 100 (team pain au chocolat)
+    tempsTimer = 60;
+    affichageScoreRank.innerHTML = `<p>Désolé ${nameUser} tu fais partie de la team pain au chocolat! ton score est de :${nombrepoints}</p>`;
+    imgShame.style.display = "block";
   }
-  setInterval(createElementChoco, 600);
-
-} else {
-
-  imgTrophy.style.display = "none";
-  imgShame.style.display = "block";
-  function createElementShame() {
-    const elementLeft = document.createElement("div");
-    elementLeft.className = "painchocolat";
-
-    marginLeft = Math.random() * 50;
-    elementLeft.style.marginLeft = marginLeft + "%";
-    elementLeft.style.animationDuration = Math.random() * 4 + 3 + "s";
-
-    const elementRight = document.createElement("div");
-    elementRight.className = "painchocolat";
-    marginLeft = Math.max(Math.random() * 60 + 20, 30);
-    elementRight.style.marginLeft = marginLeft + "%";
-    elementRight.style.animationDuration = Math.random() * 4 + 3 + 's';
-
-    body.appendChild(elementLeft);
-    body.appendChild(elementRight);
-
-    elementLeft.addEventListener("click", function () {
-      elementLeft.remove();
-    });
-    elementRight.addEventListener("click", function () {
-      elementRight.remove();
-    });
-    setTimeout(function () {
-      elementLeft.remove();
-      elementRight.remove();
-    }, 3000);
+  setTimeout(() => {
+    affichageRank.style.display = "block";
+    affichageRank.scrollIntoView();
+  }, 2000);
+  const rankFourElement = document.querySelector(".rankFour");
+  const pseudoInput = document.getElementById("pseudo");
+  // mise à jour le texte de l'élément "rankFour" avec la valeur de l'input "pseudo" et la valeur de la variable "nombrepoints"
+  rankFourElement.textContent = `${pseudoInput.value}: ${nombrepoints}pts`;
+  
+  function createPastryAndHandleClick(className) {
+    const element = createPastry(className);
+    body.appendChild(element);
+    element.addEventListener("click", () => element.remove());
+    setTimeout(() => element.remove(), 3000);
   }
-  setInterval(createElementShame, 600);
+
+  setInterval(() => createPastryAndHandleClick(nombrepoints >= 100 ? "chocolatine" : "painchocolat"), 600);
 }
 
 let compteur;
 let index = 0;
-function generateRandomAnswer(quention, choices, correctAnswer, fact) {
 
+function generateRandomAnswer(quention, choices, correctAnswer, fact) {
   compteur = index;
   index++;
-  const bodySectionQuiz = document.querySelector(".bodyquiz");
   let bodyQuiz = `<div class="quiz1 slidequiz" id="${index}">
   <div class="pictureQuestion">
       <div class="numberQuestion" id="numberid${index}">
@@ -115,85 +109,91 @@ function generateRandomAnswer(quention, choices, correctAnswer, fact) {
   <button class="answerChoice${index}">D) ${choices[3]}</button>
   </div>`;
   bodySectionQuiz.innerHTML += bodyQuiz;
-  let progressBar = document.getElementById(`idBarProgress${index}`);
-  progressBar.style.width=tempsTimer*1.666666+'%';
+   progressBar = document.getElementById(`idBarProgress${index}`);
   if (compteur > 0) {
     const animationQuiz = document.getElementById(compteur);
     animationQuiz.classList.toggle("slidequiz");
   }
   compteur++;
   const element = document.getElementById(`numberid${compteur}`);
-  element.scrollIntoView();
+  if (!endQuiz){
+    element.scrollIntoView();
+    console.log('scroll');
+  }
   // addEventListener sur chaque button de réponse
   const verificationAnswer = document.getElementsByClassName(`answerChoice${index}`);
   const arrayAnswerButton = Array.from(verificationAnswer);
   arrayAnswerButton.forEach((element, buttonIndex) => {
     element.addEventListener('click', () => {
-      arrayAnswerButton.forEach(button => {
-        button.style.pointerEvents = 'none';
-      })
+      disableButtons(arrayAnswerButton)
       // verifier la réponse de lutilisateur avec la bonne réponse de la question
       // selon résultat contour rouge ou vert sur la réponse de lutilisateur
       // compter le nombre de points
       if (choices[buttonIndex] === correctAnswer) {
         element.style.border = "4px solid green";
-        nombrepoints += 10;
+        nombrepoints += 100;
       } else {
         element.style.border = "4px solid red";
       }
-      console.log(nombrepoints);
-      // attendre 3s appel function selectionner question
+      // attendre 2s appel function selectionner question
+      tempsRestant+=1;
       setTimeout(() => {
-        selectquestionquiz(questionsVariees);
+        buttonsEnable=selectquestionquiz(questionsVariees);
+        tempsRestant=startChronometre(tempsRestant ,buttonsEnable)
       }, 2000);
 
     });
   });
+  return arrayAnswerButton;
   };
 
+function disableButtons(buttons) {
+    buttons.forEach(button => {
+      button.style.pointerEvents = 'none';
+    });
+}
 
-
-function startChronometre(temps) {
-  clearInterval(timeprogress); 
-  
+function startChronometre(temps, buttons) {
+  clearInterval(timeprogress);
   timeprogress = setInterval(() => {
     temps--;
-  
-    if (temps === 0) {
+    tempsRestant--;
+    console.log(temps);
+
+    if (temps <= 0) {
       clearInterval(timeprogress);
       tempsTimer = 0;
+      console.log('timeout temps===0');
+      showRank();
+      disableButtons(buttons); // Désactiver les boutons lorsque le temps est écoulé
     }
+    
+    // Mettez à jour la largeur de la barre de progression en fonction du temps restant
+    const progressWidth = 99 - (temps / 60) * 99; // 60 est la durée totale en secondes
+    progressBar.style.width = progressWidth + '%';
   }, 1000);
-
+  
   setTimeout(() => {
     clearInterval(timeprogress);
+    console.log('timeout settimeout');
+    temps=0;
     tempsTimer = 0;
-  }, 60000);
+    disableButtons(buttons); // Désactiver les boutons lorsque le temps est écoulé
+  }, 10000*temps);
 
-  return timeprogress;
-};
+  return temps;
+}
 
 // function selectionner une question aléatoirement dans un tableau
 function selectquestionquiz(tableauquestion) {
-  // vérifier si il reste encore du temps sinon afficher la page classement avec nombre de points réalisé
-
-  if (tempsTimer!==0){
 
     let randomIndex = Math.floor(Math.random() * tableauquestion.length);
     const { question, choices, correctAnswer, fact } =
       tableauquestion[randomIndex];
+      // supprimer la question du tableau afin de ne pas repeter la question
+      tableauquestion.splice(randomIndex, 1);
     // afficher la question, stocker la bonne reponse dans une variable
-    generateRandomAnswer(question, choices, correctAnswer, fact);
-    // supprimer la question du tableau afin de ne pas repeter la question
-    tableauquestion.splice(randomIndex, 1);
-  } else {
-    // affichage page classements
-    const affichageRank = document.getElementById('rankSelection');
-    const affichageScoreRank = document.querySelector('.detailsRankUserName');
-    affichageScoreRank.innerHTML=`<p>Félicitations ${nameUser} tu fais partie de la team chocolatine ! ton score est de :${nombrepoints}</p> `
-    affichageRank.classList.toggle("visible");
-    affichageRank.scrollIntoView();
-  }
+    return generateRandomAnswer(question, choices, correctAnswer, fact);
 }
 
 
@@ -205,13 +205,13 @@ aboutQ.addEventListener('click', function () {
 })
 
 const playAgainButton = document.getElementById("playAgainButton");
-const quizSection = document.querySelector(".bodyquiz");
+const bodyHome = document.querySelector(".home");
 
 playAgainButton.addEventListener("click", function () {
   // Affichez la section quiz quand on clique sur "retente ta chance".
-  quizSection.classList.add("visible");
+  initialisationQuiz();
+  bodyHome.scrollIntoView();
 });
-
 
 const rankFourElement = document.querySelector(".rankFour");
 const pseudoInput = document.getElementById("pseudo");
